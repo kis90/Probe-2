f58f4dcd80ba7a18379fed8fe1fe66d463df2797
kis90 <69447904+kis90@users.noreply.github.com>
2020-08-11 12:44:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector("button").onclick = match_credentials;
+var x,y;
+function match_credentials(event){
+ x= document.querySelector("#Email").value;
+ y= document.querySelector("#pass").value;
+}

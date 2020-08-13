19a4be044e2e05514d647fd2ae0e67c9e95e5acd
kis90 <69447904+kis90@users.noreply.github.com>
2020-08-13 10:01:53 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector("button").onclick = match_credentials;
-var x,y;
-function match_credentials(event){
- x= document.querySelector("#Email").value;
- y= document.querySelector("#pass").value;
-}
